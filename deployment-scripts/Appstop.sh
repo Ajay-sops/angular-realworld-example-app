8f149af62ef67c70416c8d3d7e9b280c40900c9a
Ajay-sops <92030040+Ajay-sops@users.noreply.github.com>
2023-02-15 14:13:18 +0530

    Create Appstop.sh

---
+++ deployment-scripts/Appstop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# pm2 stopall
+echo "Running pm2 processes here ======>"
+process=`pm2 jlist|jq 'length'`
+echo "Running pm2 processes ======> " $process
+if [ $process != "0" ]
+ then
+    echo "Stoping all running pm2 process"
+    pm2 stop all
+fi
