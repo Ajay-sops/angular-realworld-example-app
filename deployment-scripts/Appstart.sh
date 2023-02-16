d8ec23d88a56426f4cdbc50f55c37f8ef17939b3
Ajay-sops <92030040+Ajay-sops@users.noreply.github.com>
2023-02-16 13:02:30 +0530

    Update Appstart.sh

--- deployment-scripts/Appstart.sh
+++ deployment-scripts/Appstart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "start the node application"
-cd /app/mern-app && pm2 start "ng serve --host 0.0.0.0" --name mern-app
+cd /app/mern-app && pm2 start "ng serve --host 0.0.0.0 --disableHostCheck true" --name mern-app
